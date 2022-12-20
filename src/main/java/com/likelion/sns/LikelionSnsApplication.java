c1a8574eb595bfe7cd2fe1ba7d9c3414dd381313
mooonju <mvv0030@gmail.com>
2022-12-20 11:27:35 +0900

    first commit

---
+++ src/main/java/com/likelion/sns/LikelionSnsApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/sns/LikelionSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
