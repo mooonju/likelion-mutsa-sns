a750538630649ffe1f4f38ac780833184ce29c50
mooonju <mvv0030@gmail.com>
2022-12-20 13:41:44 +0900

    회원 가입

---
+++ src/main/java/com/likelion/sns/domaion/dto/UserJoinRequest.java
@@ -0,0 +1,11 @@
+package com.likelion.sns.domaion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserJoinRequest {
+    private String userName;
+    private String password;
+}
