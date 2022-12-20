0b54cf58e7313be07fddc5582d8fde0501e076f0
mooonju <mvv0030@gmail.com>
2022-12-20 13:38:24 +0900

    hello controller

---
+++ src/main/java/com/likelion/sns/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.likelion.sns.controller;
+
+import com.likelion.sns.domaion.dto.UserJoinRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("hello");
+    }
+}
