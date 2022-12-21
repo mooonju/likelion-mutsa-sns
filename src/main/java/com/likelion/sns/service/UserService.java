9d73b2f43cb4dd60e6271acab21736edad43c323
mooonju <mvv0030@gmail.com>
2022-12-21 11:30:04 +0900

    Security 추가 1. password encoding 2. post /users/join, /users/login 접근 제한 수정

--- src/main/java/com/likelion/sns/service/UserService.java
+++ src/main/java/com/likelion/sns/service/UserService.java
@@ -5,6 +5,7 @@ import com.likelion.sns.exception.AppException;
 import com.likelion.sns.exception.ErrorCode;
 import com.likelion.sns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
     public String join(String userName, String password) {
 
@@ -19,13 +21,13 @@ public class UserService {
         // userName 중복 체크
         userRepository.findByUserName(userName)
                 .ifPresent(user -> {
-                    throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "는 이미 있습니다");
+                    throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "이 중복됩니다");
                 });
 
         // 저장
         User user = User.builder()
                 .userName(userName)
-                .password(password)
+                .password(encoder.encode(password))
                 .build();
         userRepository.save(user);
 
