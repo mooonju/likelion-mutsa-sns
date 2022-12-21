f315cb22e1fbb05f89382e6fa5f28a28aaf2c869
mooonju <mvv0030@gmail.com>
2022-12-21 15:13:50 +0900

    [add] UserLoginRequest

---
+++ src/main/java/com/likelion/sns/domaion/dto/UserLoginRequest.java
@@ -0,0 +1,11 @@
+package com.likelion.sns.domaion.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserLoginRequest {
+    private String userName;
+    private String password;
+}
