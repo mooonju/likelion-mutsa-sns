a3c407a85d1699627d27d75b46767b3086b5b9f5
mooonju <mvv0030@gmail.com>
2022-12-22 16:58:02 +0900

    [add] 로그인 성공시 토큰 발행

--- src/main/java/com/likelion/sns/service/UserService.java
+++ src/main/java/com/likelion/sns/service/UserService.java
@@ -6,16 +6,24 @@ import com.likelion.sns.domaion.dto.UserJoinResponse;
 import com.likelion.sns.exception.AppException;
 import com.likelion.sns.exception.ErrorCode;
 import com.likelion.sns.repository.UserRepository;
+import com.likelion.sns.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String key;
+    private Long expiredTimeMs = 1000 * 60 * 60L;
 
     public UserJoinResponse join(UserJoinRequest request) {
 
@@ -39,13 +47,14 @@ public class UserService {
                 .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOTFOUND, userName + "이 없습니다"));
 
         // password 틀렸을때
-        if (!encoder.matches(selectedUser.getPassword(), password)) {
-            throw new AppException(ErrorCode.INVALID_PASSWORD, "패스워드를 잘못 입력했습니다");
+        log.info("selectedPw:{} \t pw:{}", selectedUser.getPassword(), password);
+        if(!encoder.matches(password, selectedUser.getPassword())){
+            throw new AppException(ErrorCode.INVALID_PASSWORD, "패스워드를 잘못 입력 했습니다.");
         }
 
-        //
+        // 토큰 발행
+        String token = JwtTokenUtil.createToken(selectedUser.getUserName(), key, expiredTimeMs);
 
-
-        return "token";
+        return token;
     }
 }
