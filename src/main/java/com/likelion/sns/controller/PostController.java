26e0f74eb4045a19c1f4c0cf3c024f9fdfe9441a
mooonju <mvv0030@gmail.com>
2022-12-23 15:45:14 +0900

    POST /posts 1. 게시글 등록

--- src/main/java/com/likelion/sns/controller/PostController.java
+++ src/main/java/com/likelion/sns/controller/PostController.java
@@ -7,19 +7,23 @@ import com.likelion.sns.domaion.dto.PostRequest;
 import com.likelion.sns.domaion.dto.PostResponse;
 import com.likelion.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
     private final PostService postService;
 
     @PostMapping("")
-    public ResponseEntity<Response<PostResponse>> create(@RequestBody PostRequest request, Neo4jProperties.Authentication authentication) {
-        String userName = authentication.getUsername();
+    public ResponseEntity<Response<PostResponse>> create(@RequestBody PostRequest request, Authentication authentication) {
+        String userName = authentication.getName();
+        log.info("controller userName: {}", userName);
         PostDto postDto = postService.create(request, userName);
         return ResponseEntity.ok().body(new Response<>("SUCCESS", new PostResponse("등록 완료", postDto.getId())));
     }
