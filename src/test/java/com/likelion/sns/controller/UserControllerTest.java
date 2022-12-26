a47b86123cdfb5f0088727755e7c9b42cc0800dd
mooonju <mvv0030@gmail.com>
2022-12-26 18:16:21 +0900

    User Controller Test 1.회원가입 성공 2.회원가입 실패 - userName 중복 3.로그인 성공 4-1.로그인 실패 - userName 틀림 4-2.로그인 실패 - password 틀림

--- src/test/java/com/likelion/sns/controller/UserControllerTest.java
+++ src/test/java/com/likelion/sns/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package com.likelion.sns.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.likelion.sns.domaion.dto.user.UserJoinRequest;
 import com.likelion.sns.domaion.dto.user.UserJoinResponse;
-import com.likelion.sns.domaion.dto.user.UserLoginRequest;
 import com.likelion.sns.domaion.entity.User;
 import com.likelion.sns.exception.AppException;
 import com.likelion.sns.exception.ErrorCode;
@@ -42,7 +41,7 @@ class UserControllerTest {
     @Test
     @DisplayName("회원가입 성공")
     @WithMockUser
-    void join() throws Exception {
+    void join_success() throws Exception {
         UserJoinRequest userJoinRequest = new UserJoinRequest("userName", "password");
         User user = new User(0L, userJoinRequest.getUserName(), userJoinRequest.getPassword());
         when(userService.join(any(), any())).thenReturn(UserJoinResponse.of(user));
@@ -65,26 +64,24 @@ class UserControllerTest {
     @DisplayName("회원가입 실패")
     @WithMockUser
     void join_fail() throws Exception {
-        String userName = "AAA";
-        String password = "1234";
+        UserJoinRequest userJoinRequest = new UserJoinRequest("userName", "password");
 
         when(userService.join(any(), any()))
-                .thenThrow(new RuntimeException("해당 userId가 중복됩니다."));
+                .thenThrow(new AppException(ErrorCode.DUPLICATED_USER_NAME));
 
         mockMvc.perform(post("/api/v1/users/join")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
-                .andExpect(status().isConflict());
+                .andExpect(status().is(ErrorCode.DUPLICATED_USER_NAME.getHttpStatus().value()));
     }
 
     @Test
     @DisplayName("로그인 성공")
     @WithMockUser
     void login_success() throws Exception {
-        String userName = "AAA";
-        String password = "1234";
+        UserJoinRequest userJoinRequest = new UserJoinRequest("userName", "password");
 
         when(userService.login(any(), any()))
                 .thenReturn("token");
@@ -92,17 +89,18 @@ class UserControllerTest {
         mockMvc.perform(post("/api/v1/users/login")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password))))
-                .andDo(print())
-                .andExpect(status().isOk());
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").exists())
+                .andExpect(jsonPath("$.result.jwt").exists())
+                .andDo(print());
     }
 
     @Test
     @DisplayName("로그인 실패 - userName 없음")
     @WithMockUser
     void login_fail1() throws Exception {
-        String userName = "AAA";
-        String password = "1234";
+        UserJoinRequest userJoinRequest = new UserJoinRequest("userName", "password");
 
         when(userService.login(any(), any()))
                 .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND));
@@ -110,17 +108,16 @@ class UserControllerTest {
         mockMvc.perform(post("/api/v1/users/login")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password))))
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
-                .andExpect(status().isNotFound());
+                .andExpect(status().is(ErrorCode.USERNAME_NOT_FOUND.getHttpStatus().value()));
     }
 
     @Test
     @DisplayName("로그인 실패 - password 틀림")
     @WithMockUser
     void login_fail2() throws Exception {
-        String userName = "AAA";
-        String password = "1234";
+        UserJoinRequest userJoinRequest = new UserJoinRequest("userName", "password");
 
         when(userService.login(any(), any()))
                 .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD));
@@ -128,9 +125,9 @@ class UserControllerTest {
         mockMvc.perform(post("/api/v1/users/login")
                         .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password))))
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
                 .andDo(print())
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getHttpStatus().value()));
     }
 
 }
