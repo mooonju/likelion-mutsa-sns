09d52191342f7629003bf21ae5755334188139f7
mooonju <mvv0030@gmail.com>
2022-12-29 09:49:01 +0900

    GET /hello/{num} sumOfDigit 추가

--- src/main/java/com/likelion/sns/controller/HelloController.java
+++ src/main/java/com/likelion/sns/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.likelion.sns.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,17 @@ public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("조문주");
+    }
+
+    @GetMapping("/hello/{num}")
+    public ResponseEntity<Integer> sumOfDigit(@PathVariable Integer num) {
+        int answer = 0;
+        String str = Integer.toString(num);
+
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(str.substring(i, i + 1));
+        }
+
+        return ResponseEntity.ok().body(answer);
     }
 }
